564093afad8a5ca804e4e0d8872c462122c04e6b
DOTHAIANH <97963238+DOTHAIANH@users.noreply.github.com>
2023-02-08 14:50:24 +0700

    up css

--- assets/thaianhjs/chitietamthuc.js
+++ assets/thaianhjs/chitietamthuc.js
@@ -3,6 +3,18 @@ $(document).ready(function () {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive: [
+          {
+              breakpoint: 500,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+              }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
       });
 });
 
@@ -11,5 +23,27 @@ $(document).ready(function () {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
+        arrows: true,
+        prevArrow:`<button type='button' class=''>a</button>`,
+        nextArrow:`<button type='button' class=''>a</button>`,
+        responsive: [
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+        ]
       });
 });
